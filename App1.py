7fee29bc05ca0754ba478f900d80a8e8468a06e3
Mia0301 <168523364+Mia0301@users.noreply.github.com>
2025-04-05 22:56:37 +0800

    Update App1.py

--- App1.py
+++ App1.py
@@ -9,7 +9,7 @@ import numpy as np
 from PIL import Image
 from ultralytics import YOLO
 import cv2
-model = YOLO('best (1).pt')
+model = YOLO('best (2).pt')
 
 plant_info = {
     "Adenium obesum": {
